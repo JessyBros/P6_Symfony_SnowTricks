bf914079631ad3a141d876e35ee2dc98e14fdd6d
SiProdZz <j.bros@hotmail.fr>
2021-01-07 15:50:30 +0100

    update one more time with codacy

--- public/js/managementCollectionForm.js
+++ public/js/managementCollectionForm.js
@@ -1,13 +1,13 @@
 
 function addFormToCollection($collectionHolderClass) {
     // Get the ul that holds the collection of tags "add_item_link"
-    var $collectionHolder = $('.' + $collectionHolderClass);
+    var $collectionHolder = $("." + $collectionHolderClass);
 
     // Get the data-prototype
-    var prototype = $collectionHolder.data('prototype');
+    var prototype = $collectionHolder.data("prototype");
 
     // get the new index
-    var index = $collectionHolder.data('index');
+    var index = $collectionHolder.data("index");
 
     var newForm = prototype;
 
@@ -16,7 +16,7 @@ function addFormToCollection($collectionHolderClass) {
     newForm = newForm.replace(/__name__/g, index);
 
     // increase the index with one for the next item
-    $collectionHolder.data('index', index + 1);
+    $collectionHolder.data("index", index + 1);
 
     // Display the form in the page in an li, before the "Add a tag" link li
     var $newFormLi = $('<li class="col-md-6 my-2 d-flex justify-content-center flex-column align-items-center"></li>').append(newForm);
@@ -30,7 +30,7 @@ function addTagFormDeleteLink($tagFormLi) {
     var $removeFormButton = $('<button  type="button" class="btn btn-outline-danger btn-sm mt-1"><i class="fas fa-times"></i></button>');
     $tagFormLi.append($removeFormButton);
 
-    $removeFormButton.on('click', function (e) {
+    $removeFormButton.on("click", function (e) {
         $tagFormLi.remove();
     });
 }
